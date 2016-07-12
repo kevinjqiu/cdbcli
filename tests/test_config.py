d8db3bb16d485b22d59cd0ffde571b3959cc5aa6
Kevin Jing Qiu <kevin@idempotent.ca>
2016-07-12 18:18:50 -0400

    Add tls tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -19,3 +19,13 @@ def test_cred_str_when_username_and_password_given():
 def test_cred_str_default_username_to_admin_when_only_pwd_given():
     cfg = Config('localhost', 5984, None, 'pwd', False, '')
     assert cfg.url == 'http://admin:pwd@localhost:5984/'
+
+
+def test_no_tls_means_http():
+    cfg = Config('localhost', 5984, 'admin', 'pwd', False, '')
+    assert cfg.url == 'http://admin:pwd@localhost:5984/'
+
+
+def test_tls_means_https():
+    cfg = Config('localhost', 5984, 'admin', 'pwd', True, '')
+    assert cfg.url == 'https://admin:pwd@localhost:5984/'
