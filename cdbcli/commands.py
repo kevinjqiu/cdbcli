bf7ba64511e8731252ac087eb031b50b0fa3ce2f
Kevin Jing Qiu <kevin@idempotent.ca>
2016-07-14 00:27:18 -0400

    Basic file systemm-like commands implemented

--- cdbcli/commands.py
+++ cdbcli/commands.py
@@ -36,28 +36,52 @@ def highlight(json_object):
     return pygments.highlight(unicode(formatted_json, 'UTF-8'), lexers.JsonLexer(), formatters.TerminalFormatter())
 
 
-@command_handler('USE', '(?P<database_name>[a-zA-Z0-9_-].*)')
-def use(context, couch_server, variables):
+@command_handler('ls')
+def ls(context, couch_server, variables):
+    if context.current_db is None:
+        status_code, _, response = couch_server.resource.get_json('_all_dbs')
+
+        if status_code != 200:
+            raise RuntimeError(response)
+
+        for db_name in response:
+            db = couch_server[db_name]
+            info = db.info()
+            print('{:>10} {}'.format(info['doc_count'], db_name))
+    else:
+        for doc in context.current_db:
+            print(doc)
+
+
+@command_handler('cd', '(?P<database_name>[^\s]+)')
+def cd(context, couch_server, variables):
     database_name = variables.get('database_name')
     try:
-        if not database_name:
+        if not database_name or database_name == '/' or database_name == '..':
             context.current_db = None
+        elif database_name == '-':
+            pass  # TODO: implement 'previous' database
         else:
             context.current_db = couch_server[database_name]
     except couchdb.ResourceNotFound:
         raise RuntimeError("Database '{}' does not exist".format(database_name))
 
 
-@command_handler('SHOW STATS')
+@command_handler('info')
 @require_current_db
-def show_stats(context, couch_server, variables):
+def info(context, couch_server, variables):
     info = context.current_db.info()
     print(highlight(info))
 
 
-@command_handler('GET', '(?P<doc_id>[^\s]+)')
+@command_handler('cat', '(?P<doc_id>[^\s]+)')
 @require_current_db
 def get(context, couch_server, variables):
     doc_id = variables.get('doc_id')
-    doc = context.current_db[doc_id]
+    if not doc_id:
+        raise RuntimeError('Document not found')
+
+    doc = context.current_db.get(doc_id)
+    if not doc:
+        raise RuntimeError('Document not found')
     print(highlight(doc))
