9832844d2078a1cc4f85da49a325b6dc8ab44899
Kevin Jing Qiu <kevin@idempotent.ca>
2016-07-14 23:02:40 -0400

    WordCompleter for doc ids

--- cdbcli/completer.py
+++ cdbcli/completer.py
@@ -1,21 +1,29 @@
+import functools
+
 from prompt_toolkit.contrib.completers import WordCompleter
 from prompt_toolkit.contrib.regular_languages.completion import GrammarCompleter
 from .grammar import grammar
 from .commands import COMMANDS
 
 
-def _database_name_completer(couch_server, context):
+def _fetch_db_names(couch_server, context):
     status_code, _, response = couch_server.resource.get_json('_all_dbs')
     if status_code != 200:
-        raise RuntimeError(response)
+        return []
 
-    # Need to update WordCompleter so it calls a function to obtain a list of
-    # words rather than a static list
-    return WordCompleter(response)
+    return response
+
+
+def _fetch_doc_ids(couch_server, context):
+    if context.current_db is None:
+        return []
+
+    return list(context.current_db)
 
 
 def get_completer(couch_server, context):
     return GrammarCompleter(grammar, {
         'command': WordCompleter(COMMANDS.keys()),
-        'database_name': _database_name_completer(couch_server, context),
+        'database_name': WordCompleter(functools.partial(_fetch_db_names, couch_server, context)),
+        'doc_id': WordCompleter(functools.partial(_fetch_doc_ids, couch_server, context)),
     })
