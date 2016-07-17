2bd7a153afd2af34caf3eb38f1d93c135f7266e3
Kevin Jing Qiu <kevin@idempotent.ca>
2016-07-17 18:30:36 -0400

    RuntimeError

--- tests/integration/test_repl.py
+++ tests/integration/test_repl.py
@@ -1,10 +1,13 @@
+import pytest
+
 from cdbcli.repl import eval_
 from cdbcli.environment import Environment
 from fixtures import *  # noqa
 
 
 def test_cd_on_nonexistent_db_raises_error(environment, couch_server):
-    pass
+    with pytest.raises(RuntimeError):
+        eval_(environment, couch_server, 'cd test')
 
 
 def test_cd_changes_current_db_in_environment(environment, couch_server):
