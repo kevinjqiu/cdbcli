773fac95c89c025f312645e4d4ce53c60c5e493a
Kevin Jing Qiu <kevin@idempotent.ca>
2016-07-20 00:29:02 -0400

    Test fetch ids

--- tests/integration/test_completer.py
+++ tests/integration/test_completer.py
@@ -34,3 +34,17 @@ def test_fetch_db_names(environment, couch_server):
     couch_server.create('test3')
     dbnames = completer.fetch_db_names(environment, couch_server)
     assert set(['test1', 'test2', 'test3', '_replicator', '_users']) == set(dbnames)
+
+
+def test_fetch_doc_ids_no_current_db(environment, couch_server):
+    doc_ids = completer.fetch_doc_ids(environment, couch_server)
+    assert [] == doc_ids
+
+
+def test_fetch_doc_ids_with_current_db(environment, couch_server):
+    db = couch_server.create('test')
+    db.save(get_user_doc('john', 'smith'))
+    db.save(get_user_doc('jane', 'doe'))
+    environment.current_db = db
+    doc_ids = completer.fetch_doc_ids(environment, couch_server)
+    assert set(['jane.doe', 'john.smith']) == set(doc_ids)
