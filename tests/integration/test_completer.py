13ab94af3d42d70b8d6fd929d2e0762e2c9baf5e
Kevin Jing Qiu <kevin@idempotent.ca>
2016-07-20 00:26:11 -0400

    Add fetch db names test

--- tests/integration/test_completer.py
+++ tests/integration/test_completer.py
@@ -26,3 +26,11 @@ def test_fetch_view_ids_with_current_db(environment, couch_server):
     environment.current_db = db
     view_paths = completer.fetch_view_ids(environment, couch_server)
     assert set(['_design/users']) == set(view_paths)
+
+
+def test_fetch_db_names(environment, couch_server):
+    couch_server.create('test1')
+    couch_server.create('test2')
+    couch_server.create('test3')
+    dbnames = completer.fetch_db_names(environment, couch_server)
+    assert set(['test1', 'test2', 'test3', '_replicator', '_users']) == set(dbnames)
